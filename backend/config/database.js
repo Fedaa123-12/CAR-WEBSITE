0b4613bfe2ddff91a64b5cb91c68fb1e5d7f1e13
Fedaa123-12 <127947260+Fedaa1234@users.noreply.github.com>
2024-03-31 03:27:13 +0100

    .

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ import mysql from "mysql2";
 const db = mysql.createConnection({
     host: "sql8.freesqldatabase.com",
     user: "sql8695053",
-    password: "",
+    password: "CiE24Lf8kJ",
     database: "sql8695053",
     //default port 3306
 });
