ce82645f39385cc604b8b16b6a8b9f11ae9e00d8
Fedaa123-12 <127947260+Fedaa1234@users.noreply.github.com>
2024-04-05 03:37:40 +0100

    cards setup and navbar added

--- frontend/src/components/carList.vue
+++ frontend/src/components/carList.vue
@@ -1,30 +1,5 @@
-<template>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Brand</th>
-                <th scope="col">Model</th>
-                <th scope="col">Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item in items" :key="item.ID">
-                <td scope="col">
-                    {{ item.Name }}
-                </td>
-                <td scope="col">
-                    {{ item.Model }}
-                </td>
-                <td :id = item.ID  scope="col" @click = >
-                    <button>Delete</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</template>
-
-
 <script>
+
 import axios from "axios";
 export default {
     data() {
@@ -45,9 +20,59 @@ export default {
                 console.log(err);
             }
         },
-        async deleteCars(){
-       
+        async deleteCars(index, id) {
+            try {
+                await axios.delete("https://carwebsiteapi.cyclic.app/deleteById?id=" + id);
+                this.items.splice(index, 1);
+            } catch (err) {
+                console.log(err);
+            }
         }
     }
 };
 </script>
+
+<template>
+
+    <main>
+        
+        <div class="album py-5 bg-light">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div class="col" v-for="item in items" :key="item.ID">
+                        <div class="card shadow-sm">
+                            
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+                                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                                preserveAspectRatio="xMidYMid slice" focusable="false">
+                                <title>Placeholder</title>
+                                <image :xlink:href="item.Image" width="100%" height="100%" />
+                                <!-- <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%"
+                                    fill="#eceeef" dy=".3em">{{ item.Image }}</text> -->
+                            </svg>
+                            <div class="card-body">
+                                
+                                <p class="card-text">
+                                <strong>Brand:</strong> {{ item.Name }}<br>
+                                <strong>Model:</strong> {{ item.Model }}
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                        
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary" @click=deleteCars(index,item.ID)>Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import index from '../views/index.vue'
-
 const routes = [
   {
     path: "/",
     name : "index",
-    component : index
+    component : () => import('../views/index.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
 ]
-
-
-
-
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
 export default router
