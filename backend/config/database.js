3a030dacf92bcf95dd8197f1e4a8f48b40a97336
Fedaa123-12 <127947260+Fedaa1234@users.noreply.github.com>
2024-04-07 03:58:20 +0100

    removed unused notes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,26 +1,6 @@
 import mysql from "mysql2";
 
 
-// //connect
-// const db = mysql.createConnection({
-//     host: "sql8.freesqldatabase.com",
-//     user: "sql8695053",
-//     password: "CiE24Lf8kJ",
-//     database: "sql8695053",
-//     //default port 3306
-// });
-
-
-
-
-
-
-
-
-// export default db;
-
-
-
 // Create a connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
